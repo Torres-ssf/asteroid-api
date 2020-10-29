b120cbab63b6483169b35e6c16dfff1306ca05b8
Torres-ssf <sergio.uft@gmail.com>
2020-10-29 18:55:26 -0300

    chore: update IUsersRepository interface

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -3,8 +3,8 @@ import ICreateUserDTO from '../dtos/ICreateUserDTO';
 
 interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<User>;
-  findById(id: string): Promise<User>;
-  findByEmail(email: string): Promise<User>;
+  findById(id: string): Promise<User | undefined>;
+  findByEmail(email: string): Promise<User | undefined>;
 }
 
 export default IUsersRepository;
