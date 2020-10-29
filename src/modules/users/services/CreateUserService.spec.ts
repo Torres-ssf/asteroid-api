af34841fe25c063254d6ad27f944e254682f24a1
Torres-ssf <sergio.uft@gmail.com>
2020-10-29 19:11:58 -0300

    test: verify existent user at CreateUserService

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,5 +1,6 @@
 import CreateUserService from './CreateUserService';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import AppError from '../../../shared/errors/AppError';
 
 describe('CreateUser', () => {
   let fakeUsersRepository: FakeUsersRepository;
@@ -12,7 +13,7 @@ describe('CreateUser', () => {
     createUserService = new CreateUserService(fakeUsersRepository);
   });
 
-  it('should create a user', async () => {
+  it('should create a new user', async () => {
     const user = await createUserService.execute({
       name: 'Paul',
       email: 'paul@email.com',
@@ -28,5 +29,21 @@ describe('CreateUser', () => {
     expect(user).toHaveProperty('id');
     expect(user).toHaveProperty('created_at');
     expect(user).toHaveProperty('updated_at');
+  });
+
+  it('should not create a new user with an email already in use', async () => {
+    await createUserService.execute({
+      name: 'Paul',
+      email: 'paul@email.com',
+      password: '123456',
+    });
+
+    await expect(
+      createUserService.execute({
+        name: 'Paul',
+        email: 'paul@email.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
