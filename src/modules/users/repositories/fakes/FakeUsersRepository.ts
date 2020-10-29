bf5a69b245aa073e0cf23da697d594845bae9dea
Torres-ssf <sergio.uft@gmail.com>
2020-10-29 16:54:17 -0300

    feat: implement create user method at FakeUsersRepository

---
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,21 @@
+import { v4 } from 'uuid';
+import User from '../../entities/IUser';
+import ICreateUserDTO from '../../dtos/ICreateUserDTO';
+import IUsersRepository from '../IUsersRepository';
+
+class FakeUsersRepository implements IUsersRepository {
+  async create(userData: ICreateUserDTO): Promise<User> {
+    const { name, email, password } = userData;
+
+    const user = {
+      id: v4(),
+      name,
+      email,
+      password,
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    return user;
+  }
+}
