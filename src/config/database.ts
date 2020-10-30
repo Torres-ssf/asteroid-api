56f9a0cf7b226b5fbe1fdb4acc72c89cb56f9e93
Torres-ssf <sergio.uft@gmail.com>
2020-10-30 17:05:06 -0300

    feat: create set up database file at config domain

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,5 +1,7 @@
 import Mongoose from 'mongoose';
 
+import '@modules/users/infra/mongoose/entities/MongooseUser';
+
 Mongoose.connect('mongodb://localhost:27017/mongodb-test', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
