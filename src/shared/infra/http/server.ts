1fe56aa45f9b2abc646eca4e4b0f235e3d63e485
Torres-ssf <sergio.uft@gmail.com>
2020-10-30 21:25:51 -0300

    feat: use express-async-errors to deal with unhandled promises rejection

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,9 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { json, Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
+import AppError from '@shared/errors/AppError';
 
 import '@config/database';
 import '@shared/container';
@@ -9,11 +12,21 @@ import appRoutes from './routes';
 
 const app = express();
 
-app.use(express.json());
+app.use(json());
 
 app.use(appRoutes);
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res
+      .status(err.statusCode)
+      .json({ status: 'error', message: err.message });
+  }
+
+  return res
+    .status(500)
+    .json({ status: 'error', message: 'Internal server error' });
+});
 
 app.listen(3333, () => {
   console.log('🚀 Server launched at 3333');
