a0df410a402cede9338fc11eb27aa7e030cbc681
Torres-ssf <sergio.uft@gmail.com>
2020-10-31 15:19:23 -0300

    feat: inject TokenProvider dependency at providers/index.ts file

--- src/modules/users/providers/index.ts
+++ src/modules/users/providers/index.ts
@@ -3,4 +3,9 @@ import { container } from 'tsyringe';
 import IHashProvider from './HashProvider/models/IHashProvider';
 import BCryptHashProvider from './HashProvider/implementations/BCryptHashProvider';
 
+import ITokenProvider from './TokenProvider/models/ITokenProvider';
+import JWTTokenProvider from './TokenProvider/implementations/JWTTokenProvider';
+
 container.registerSingleton<IHashProvider>('HashProvider', BCryptHashProvider);
+
+container.registerSingleton<ITokenProvider>('TokenProvider', JWTTokenProvider);
