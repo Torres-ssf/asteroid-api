836195c6960baabc7609482dacaaa911f24f0f6a
Torres-ssf <sergio.uft@gmail.com>
2020-11-01 17:09:22 -0300

    feat: import auth config at AuthenticateUserService

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
+import authConfig from '@config/auth';
 import User from '../entities/IUser';
 import IUsersRepository from '../repositories/IUsersRepository';
 
@@ -54,10 +55,12 @@ class AuthenticateUserService {
       throw new AppError('Wrong user/password combination');
     }
 
+    const { secret, expiration } = authConfig;
+
     const token = this.tokenProvider.generate({
       payload: { userId: user.id },
-      secret: 'developmentPurposeSecret',
-      expiresIn: '1d',
+      secret,
+      expiresIn: expiration,
     });
 
     return {
