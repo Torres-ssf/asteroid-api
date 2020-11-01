78a40d3217ea9a925072c5306848fb5fb3cfb011
Torres-ssf <sergio.uft@gmail.com>
2020-11-01 17:14:13 -0300

    feat: import sessions routes to index sessions file

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
 import usersRoutes from '@modules/users/infra/http/routes/users.routes';
+import sessionsRoutes from '@modules/users/infra/http/routes/sessions.routes';
 
 const appRoutes = Router();
 
 appRoutes.use('/users', usersRoutes);
+
+appRoutes.use('/sessions', sessionsRoutes);
 
 export default appRoutes;
