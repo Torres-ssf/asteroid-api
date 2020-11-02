2e31ec539bf783aa88e9afa4b1ac2b32bc3fbffc
Torres-ssf <sergio.uft@gmail.com>
2020-11-02 19:25:33 -0300

    test: improve AuthenticateUserService

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -6,6 +6,12 @@ import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import AuthenticateUser from './AuthenticateUserService';
 import FakeTokenProvider from '../providers/TokenProvider/fakes/FakeTokenProvider';
 
+interface IUserCredentials {
+  name: string;
+  email: string;
+  password: string;
+}
+
 describe('AuthenticateUser', () => {
   let authenticateUser: AuthenticateUser;
 
@@ -14,6 +20,8 @@ describe('AuthenticateUser', () => {
   let fakeHashProvider: FakeHashProvider;
 
   let fakeTokenProvider: FakeTokenProvider;
+
+  let userCredentials: IUserCredentials;
 
   beforeEach(() => {
     fakeUserRepository = new FakeUserRepository();
@@ -25,86 +33,83 @@ describe('AuthenticateUser', () => {
       fakeHashProvider,
       fakeTokenProvider,
     );
+
+    userCredentials = {
+      name: 'Paul',
+      email: 'paul@email.com',
+      password: '123456',
+    };
   });
 
   it('should not be able to authenticate with unregistered email', async () => {
     await expect(
       authenticateUser.execute({
-        email: 'not-registered-user@email.com',
+        email: 'unknown@email.com',
         password: '123456',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
 
   it('should not be able to authenticate with wrong password', async () => {
-    const userPassword = '123456';
-
-    const user = await fakeUserRepository.create({
-      name: 'Paul',
-      email: 'paul@email.com',
-      password: userPassword,
-    });
+    const user = await fakeUserRepository.create(userCredentials);
 
     await expect(
       authenticateUser.execute({
         email: user.email,
-        password: '121212',
+        password: 'wrong-password',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
 
   it('should compare the hashed password with given password', async () => {
-    const user = await fakeUserRepository.create({
-      name: 'Paul',
-      email: 'paul@email.com',
-      password: '123456',
-    });
+    const user = await fakeUserRepository.create(userCredentials);
 
     const spy = jest.spyOn(fakeHashProvider, 'compare');
 
     await authenticateUser.execute({
-      email: 'paul@email.com',
-      password: '123456',
+      email: userCredentials.email,
+      password: userCredentials.password,
     });
 
-    expect(spy).toHaveBeenCalledWith('123456', user.password);
+    expect(spy).toHaveBeenCalledWith(userCredentials.password, user.password);
   });
 
   it('should generate an authentication token when right credentials are provided', async () => {
-    const user = await fakeUserRepository.create({
-      name: 'Paul',
-      email: 'paul@email.com',
-      password: '123456',
-    });
+    await fakeUserRepository.create(userCredentials);
 
     const spy = jest.spyOn(fakeTokenProvider, 'generate');
 
     await authenticateUser.execute({
-      email: user.email,
-      password: '123456',
+      email: userCredentials.email,
+      password: userCredentials.password,
     });
 
     expect(spy).toHaveBeenCalled();
   });
 
-  it('should authenticate a user when right credentials are provided', async () => {
-    await fakeUserRepository.create({
-      name: 'Paul',
-      email: 'paul@email.com',
-      password: '123456',
+  it('should return the token that was genereted by the TokenProvider', async () => {
+    await fakeUserRepository.create(userCredentials);
+
+    const spy = jest.spyOn(fakeTokenProvider, 'generate');
+
+    const res = await authenticateUser.execute({
+      email: userCredentials.email,
+      password: userCredentials.password,
     });
 
+    expect(spy).toHaveReturnedWith(res.token);
+  });
+
+  it('should authenticate and return user data and the authentication token', async () => {
+    await fakeUserRepository.create(userCredentials);
+
     const { user, token } = await authenticateUser.execute({
-      email: 'paul@email.com',
-      password: '123456',
+      email: userCredentials.email,
+      password: userCredentials.password,
     });
 
     expect(typeof token).toBe('string');
 
-    expect(user).toMatchObject({
-      name: 'Paul',
-      email: 'paul@email.com',
-      password: '123456',
-    });
+    expect(user).toMatchObject(userCredentials);
   });
 });
