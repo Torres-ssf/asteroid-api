5d86e76215a090d3e43e84cc84c6d17986072881
Torres-ssf <sergio.uft@gmail.com>
2020-11-02 20:11:05 -0300

    feat: add profile routes to index route file

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,11 +2,14 @@ import { Router } from 'express';
 
 import usersRoutes from '@modules/users/infra/http/routes/users.routes';
 import sessionsRoutes from '@modules/users/infra/http/routes/sessions.routes';
+import profileRoutes from '@modules/users/infra/http/routes/profile.routes';
 
 const appRoutes = Router();
 
 appRoutes.use('/users', usersRoutes);
 
 appRoutes.use('/sessions', sessionsRoutes);
+
+appRoutes.use('/profile', profileRoutes);
 
 export default appRoutes;
